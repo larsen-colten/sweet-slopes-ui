bf50a0c2a5bd4fd852766182888196ee82769185
larsen-colten <larsen.colten@gmail.com>
2024-05-13 15:48:14 -0600

    Add changes to cards and cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,8 +4,7 @@ import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import { CreditCard, PaymentForm } from "react-square-web-payments-sdk";
 import { createOrder, payOrder, submitPayment } from '../actions/actions';
-import { CreatePaymentRequest, PayOrderRequest } from 'square';
-import { randomUUID } from 'crypto';
+import { CreateOrderRequest, CreatePaymentRequest, PayOrderRequest } from 'square';
 import { CartContext } from '../CartContext';
 
 enum Pages {
@@ -14,8 +13,13 @@ enum Pages {
 }
 
 export default function CartPage() {
-    const [pickup, setPickup] = useState(false);
-    const [firstTab, setFirstTab] = useState(true);
+    const [email, setEmail] = useState('');
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
+
     const { order } = useContext(CartContext);
     const [page, setPage] = useState(Pages.BasicInfo);
 
@@ -24,20 +28,33 @@ export default function CartPage() {
             sourceId: token.token,
             idempotencyKey: "",
             amountMoney: {
-                amount: BigInt(100),
+                amount: BigInt(order.totalMoney?.amount || 0),
                 currency: "USD"
             }
         };
         const resultSubmitPayment = await submitPayment(createPaymentRequest);
-        console.log(resultSubmitPayment);
 
         if (resultSubmitPayment) {
-            var createOrderRequest = {
+            order.fulfillments = [{
+                deliveryDetails: {
+                    recipient: {
+                        address: {
+                            addressLine1: address1,
+                            addressLine2: address2,
+                            locality: city,
+                            administrativeDistrictLevel1: state,
+                            postalCode: zip
+                        },
+                        displayName: email
+                    }
+                }
+            }];
+
+            var createOrderRequest: CreateOrderRequest = {
                 order: order,
                 idempotencyKey: "",
             }
             const resultCreateOrder = await createOrder(createOrderRequest);
-            console.log(resultCreateOrder);
 
             if (resultCreateOrder) {
                 var payOrderRequest: PayOrderRequest = {
@@ -47,11 +64,9 @@ export default function CartPage() {
 
                 const resultPayOrder = await payOrder(resultCreateOrder.id, payOrderRequest);
                 if (resultPayOrder) {
-                    console.log(resultPayOrder);
                     console.log('Payment successful');
                 }
             }
-
         }
     }
 
@@ -59,93 +74,145 @@ export default function CartPage() {
         <>
             <Title title={"Cart"} />
 
-            {order.lineItems?.map((item, index) => {
-                return (
-                    <div key={index} className="card bg-base-100 shadow-xl w-950">
-                        <div className="card-body">
-                            <h2 className="card-title">{item.name}</h2>
-                            <p>{item.quantity}</p>
-                        </div>
-                    </div>
-                )
-            })}
-
             {/* Basic information */}
-            {page == Pages.BasicInfo &&
-                <>
-                    <div className="label">
-                        <span className="label-text">Email</span>
+            <div className="grid grid-cols-10 py-20">
+                <div className="col-start-3 col-end-9 bg-white p-10 rounded-3xl">
+                    <div className=''>
+                        {page == Pages.BasicInfo &&
+                            <>
+                                <div className="label">
+                                    <span className="label-text">Email</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input type="text"
+                                        className="grow"
+                                        placeholder="sweetslopes@gmail.com"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </label>
+
+                                {/* Address */}
+                                <div className="label">
+                                    <span className="label-text">Adderess 1</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input
+                                        type="text"
+                                        className="grow"
+                                        placeholder="123 N ABC W"
+                                        value={address1}
+                                        onChange={(e) => setAddress1(e.target.value)}
+                                    />
+                                </label>
+
+                                {/* Address 2 */}
+                                <div className="label">
+                                    <span className="label-text">Address 2</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input type="text"
+                                        className="grow"
+                                        placeholder="Apt 101"
+                                        value={address2}
+                                        onChange={(e) => setAddress2(e.target.value)}
+                                    />
+                                </label>
+
+                                {/* City */}
+                                <div className="label">
+                                    <span className="label-text">City</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input
+                                        type="text"
+                                        className="grow"
+                                        placeholder="Lehi"
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                    />
+                                </label>
+
+                                {/* State */}
+                                <div className="label">
+                                    <span className="label-text">State</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input
+                                        type="text"
+                                        className="grow"
+                                        placeholder="UT"
+                                        value={state}
+                                        onChange={(e) => setState(e.target.value)}
+                                    />
+                                </label>
+
+                                {/* Zip */}
+                                <div className="label">
+                                    <span className="label-text">Zip</span>
+                                </div>
+                                <label className='input input-bordered flex items-center gap-2 bg-white'>
+                                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
+                                    <input
+                                        type="text"
+                                        className="grow"
+                                        placeholder="84043"
+                                        value={zip}
+                                        onChange={(e) => setZip(e.target.value)}
+                                    />
+                                </label>
+
+                                <button className="btn btn-primary mt-6" onClick={() => setPage(Pages.Review)}>Review</button>
+                            </>
+                        }
+
+                        {/* Review cart */}
+                        {page == Pages.Review &&
+                            <>
+                                <div className="overflow-x-auto p-5">
+                                    <table className="table w-full">
+                                        <thead>
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Price</th>
+                                                <th>Quantity</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {order.lineItems?.map((item, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>{item.name}</td>
+                                                        <td>{(((Number(item.basePriceMoney?.amount) || 0) / 100) * +item.quantity).toFixed(2)}${ }</td>
+                                                        <td>{item.quantity}</td>
+                                                    </tr>
+                                                )
+                                            })}
+                                        </tbody>
+                                    </table>
+                                </div>
+
+
+
+                                <PaymentForm
+                                    applicationId={"sandbox-sq0idb-GaJ-_gQkxXfZwSkmvRtXeA"}
+                                    locationId={"L18ARQTAE5CE2"}
+                                    cardTokenizeResponseReceived={async (token: any) => {
+                                        pay(token);
+                                    }}
+                                >
+                                    <CreditCard />
+                                </PaymentForm>
+                            </>
+                        }
                     </div>
-                    <label className='input input-bordered flex items-center gap-2'>
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
-                        <input type="text" className="grow" placeholder="sweetslopes@gmail.com" />
-                    </label>
-
-                    {(pickup == false) &&
-                        <>
-                            <label>
-                                Deliver to
-                            </label>
-
-
-                            {/* Address */}
-                            <div className="label">
-                                <span className="label-text">Adderess</span>
-                            </div>
-                            <label className='input input-bordered flex items-center gap-2'>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
-                                <input type="text" className="grow" placeholder="123 N ABC W" />
-                            </label>
-
-                            {/* Address 2 */}
-                            <div className="label">
-                                <span className="label-text">Address 2</span>
-                            </div>
-                            <label className='input input-bordered flex items-center gap-2'>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
-                                <input type="text" className="grow" placeholder="Apt 101" />
-                            </label>
-
-                            {/* City */}
-                            <div className="label">
-                                <span className="label-text">City</span>
-                            </div>
-                            <label className='input input-bordered flex items-center gap-2'>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
-                                <input type="text" className="grow" placeholder="Lehi" />
-                            </label>
-
-                            {/* Zip */}
-                            <div className="label">
-                                <span className="label-text">Zip</span>
-                            </div>
-                            <label className='input input-bordered flex items-center gap-2'>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg> */}
-                                <input type="text" className="grow" placeholder="84043" />
-                            </label>
-
-
-                        </>
-                    }
-
-                    <button className="btn btn-primary" onClick={() => setPage(Pages.Review)}>Review</button>
-                </>
-            }
-
-            {/* Review cart */}
-            {page == Pages.Review &&
-                <>
-                    <PaymentForm
-                        applicationId={"sandbox-sq0idb-GaJ-_gQkxXfZwSkmvRtXeA"}
-                        locationId={"L18ARQTAE5CE2"}
-                        cardTokenizeResponseReceived={async (token: any) => {
-                            pay(token);
-                        }}
-                    >
-                        <CreditCard />
-                    </PaymentForm>
-                </>
-            }
+                </div>
+            </div >
         </>
     )
 }
--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -12,7 +12,7 @@ export default function ProductCard(props: ProductCardProps) {
     const [name, setName] = React.useState<string>('');
     const [price, setPrice] = React.useState<number>(0);
     const [description, setDescription] = React.useState<string>('');
-    const { addToCart } = useContext(CartContext);
+    const { order, addToCart } = useContext(CartContext);
 
     useEffect(() => {
         setName(props.product.itemData?.name || '');
@@ -20,20 +20,38 @@ export default function ProductCard(props: ProductCardProps) {
         setDescription(props.product.itemData?.description || '');
     }, []);
 
-    function AddToCart() {
-        var variationId = props.product.itemData?.variations?.[0].id;
-        const lineItem = {
-            catalogObjectId: variationId,
-            quantity: '1'
+    function truncate(str: string, num: number) {
+        if (str.length <= num) {
+            return str;
         }
-        addToCart(lineItem);
+        return str.slice(0, num) + '...';
+    }
+
+    function AddToCart() {
+        const lineItem = {
+            catalogObjectId: props.product.itemData?.variations?.[0].id,
+            quantity: '1',
+            basePriceMoney: props.product.itemData?.variations?.[0].itemVariationData?.priceMoney,
+            name: name,
+        }
+
+        // Check if the item already exists in the cart
+        const existingItem = order.lineItems?.find(item => item.catalogObjectId === lineItem.catalogObjectId);
+
+        if (existingItem) {
+            // If the item already exists, increment the quantity
+            existingItem.quantity = (parseInt(existingItem.quantity) + 1).toString();
+        } else {
+            // If the item doesn't exist, add it to the cart
+            addToCart(lineItem);
+        }
     }
 
     return (
-        <div className='card w-950 bg-neutral shadow-xl flex-none'>
+        <div className='card w-950 bg-neutral shadow-xl flex-none max-w-[20rem]'>
             <figure className="px-10 pt-10"><Image className="gap-10" src={props.imageURL} width={200} height={200} alt="cake" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+                <h2 className="card-title">{truncate(name, 25)}</h2>
                 <h3 className="card-cost">{price.toFixed(2)} $</h3>
                 <p>{description}</p>
                 <div className="card-actions justify-end">
